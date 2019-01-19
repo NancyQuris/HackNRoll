921f9ea57c4c9bc6571038a5656348158bdb2c9c
lancelotwillow <lancelotwillow@gmail.com>
2019-01-19 14:55:06 +0800

    pause vue

--- resources/views/chats/index.blade.php
+++ resources/views/chats/index.blade.php
@@ -1,27 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<p1>wlcome to chat</p1>
+
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading" style="background-color:#F0EDB3">Chats</div>
-
-                <div class="panel-body" style="background-color:#F0EFDB">
-                    something2
-                    <chat-messages :messages="messages"></chat-messages>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Chats</div>
+                <div class="card-body">
+                    Chats
                 </div>
-                <div class="panel-footer" style="background-color:#DCD77E">
-                    form
-                    <chat-form
-                        v-on:messagesent="addMessage"
-                        :user="{{ Auth::user() }}"
-                    ></chat-form>
-
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">Users</div>
+                <div class="card-body">
+                    Users
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
+
+
