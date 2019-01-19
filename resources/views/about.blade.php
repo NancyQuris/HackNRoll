efd767d3ddfdec8829faeb93b153c09731e766af
NancyQuris <liuyingnan1999@gmail.com>
2019-01-20 00:27:45 +0800

    update about us page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,13 +6,15 @@
 <link rel="stylesheet" href="/css/initial.css">
 
 <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
-  <div class="w3-display-topleft w3-padding-large w3-xlarge">
-    Logo
-  </div>
   <div class="w3-display-middle">
-    <h1 class="w3-jumbo w3-animate-top">I will not tell U who I AM</h1>
-    <div class="w3-center w3-jumbow3-animate-top">
-    <h2><a href="/">Go back to home</a></h2>
+    <h1 class="w3-jumbo w3-animate-top">About Us</h1>
+    <p style="font-size:15pt">Hello, welcome to Like-minded Fellow! This website aims to help you find people that have similar personality to you! By conducting our personality test, you will be able to have an insightful understanding of your own personality. Based on your personality, you can find online chat mates that have similar personality. 
+		Start experiencing this website by <a href="/register" style="color:CornflowerBlue">register</a> or <a href="/login" style="color: CornflowerBlue">login</a> first! <br>
+		Happy exploring and chatting!<br><br>
+		Developed with 💓 by <a href="https://github.com/lancelotwillow" style="color:gold">Tianyang</a>, <a href="https://github.com/Drowningfishy" style="color:gold">Xinru</a>, <a href="https://github.com/Liuyy99" style="color:gold">Yiyang</a>, and <a href="https://github.com/NancyQuris" style="color:gold">Yingnan</a>
+	</p>
+    <div class="w3-jumbo w3-animate-top">
+    <h2><a href="/">go back to home</a></h2>
   </div>
   </div>
 </div>
