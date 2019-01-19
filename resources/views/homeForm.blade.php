b6f0b3972a36c1c03c2098e01c00397ab149170f
Drowningfishy <e0253676@u.nus.edu>
2019-01-19 18:14:20 +0800

    homeform formatting

--- resources/views/homeForm.blade.php
+++ resources/views/homeForm.blade.php
@@ -7,79 +7,27 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="/css/home.css">
 
-  {{-- <!-- Navbar (sit on top) -->
-  <div class="w3-top">
-    <div class="w3-bar" id="myNavbar">
-      <a href="#home" class="w3-bar-item w3-button"><i class="fa fa-home"></i>HOME</a>
-      <a href="#about" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> ABOUT</a>
-      <a href="#contact" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-phone"></i> START CHATTING</a>
-    </div>
-
-    <!-- Navbar on small screens -->
-    <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
-      <a href="#about" class="w3-bar-item w3-button">ABOUT</a>
-      <a href="#contact" class="w3-bar-item w3-button">START CHATTING</a>
-    </div>
-  </div> --}}
-  
-
-  <!-- Logo -->
-  <div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
-    <div class="w3-display-middle" style="white-space:nowrap;">
-    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity"><?php echo strtoupper(auth()->user()->name."'s") ?> <span class="w3-hide-small">HOME</span> PAGE</span>
+  <div class = "container">
     <br>
-    <a href=''> edit background</a>
+    <li><a href=''> Click to upload background picture</a></li>
     <br>
-    <a href=''> edit profile photo</a>
-    </div>
-  </div>
+    <li><a href=''> Click to upload profile photo</a></li>
+    <br>
+    <li><a href="/testform">Click to start to discover your personality type!</a></li>
+    <br>
+   
+      <div>
 
-  <!-- Container (About Section) -->
-  <div class="w3-content w3-container w3-padding-64" id="about">
-    <h3 class="w3-center">ABOUT ME</h3>
-    <div class="w3-row">
-      <div class="w3-col m6 w3-center w3-padding-large">
-        <img src="/css/self-photo.jpg" class="w3-round w3-image w3-opacity w3-hover-opacity-off" alt="Photo of Me" width="500" height="333">
-      </div>
-
-      <!-- Hide this text on small devices -->
-      <div class="w3-col m6 w3-hide-small w3-padding-large">
+       <li>Write your self_introduction below</li>
        <form method="POST" actions="/homeForm">
           @csrf
-          <input type="text" name="self_introduction" placeholder="self introduction">
+          <br>
+          <input type="textarea" name="self_introduction" class = "form-control" placeholder="self_introduction" >
           <br >
-          <button type="submit">submit</button>
+          <button type="submit" class = "btn btn-primary">Submit</button>
         </form>
       </div>
-    </div>
-    <p class="w3-large w3-center w3-padding-16">Personality analysis:</p>
-    <a href="">start to discover your persoanlity!</a>
-    </div>
   </div>
 
-  <!-- Footer -->
-  <footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off">
-    <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
-    <div class="w3-xlarge w3-section">
-      <i class="fa fa-facebook-official w3-hover-opacity"></i>
-      <i class="fa fa-instagram w3-hover-opacity"></i>
-      <i class="fa fa-snapchat w3-hover-opacity"></i>
-      <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-      <i class="fa fa-twitter w3-hover-opacity"></i>
-      <i class="fa fa-linkedin w3-hover-opacity"></i>
-    </div>
-
-  <script>
-  // Change style of navbar on scroll
-  window.onscroll = function() {myFunction()};
-  function myFunction() {
-      var navbar = document.getElementById("myNavbar");
-      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-          navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
-      } else {
-          navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
-      }
-  }
-  </script>
 @endsection
 
